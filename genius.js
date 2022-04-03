2f9bcee04797fe813fbbf53cf6a029a7f6d0b7fe
Matheus Silva <matheus_lsilva@hotmail.com>
2022-04-03 19:27:50 -0300

    Atualizacao do layout

--- genius.js
+++ genius.js
@@ -1,38 +1,51 @@
 function modal() {
+  
   const main = document.getElementById("main");
-  const div = document.createElement("div");
-  div.classList.add("divModal");
-  const h1 = document.createElement("h1");
-  h1.innerText = "Bem vindo(a) ao Genius";
-  const form = document.createElement("form");
-  const input = document.createElement("input");
-  input.placeholder = "Insira seu nome Aqui";
-  const button = document.createElement("button");
-  button.id = "button"; //criação da primeira Pagina com input
-  button.innerText = "Enviar";
-  main.appendChild(div);
-  div.appendChild(h1);
-  div.appendChild(form);
-  form.appendChild(input);
-  form.appendChild(button);
+  const divModal = document.createElement("div")
+  divModal.classList.add("divModal")
+  const h1 = document.createElement("h1")
+  h1.innerText = "seja bem vindo(a) ao genius,"
+  const h2 = document.createElement("h2")
+  const form = document.createElement("form")
+  const input = document.createElement("input")
+  input.id = "change"
+  input.placeholder = "Insira seu nome aqui"
+  input.maxLength = 10
+  const button = document.createElement("button")
+  button.innerText ="JOGAR!"
+  button.id = "button"
+
+  form.appendChild(input)
+  form.appendChild(button)
+  divModal.appendChild(h1)
+  divModal.appendChild(h2)
+  divModal.appendChild(form)
+  main.appendChild(divModal) 
+  
+  const input2 = document.getElementById("change")
+  input2.addEventListener('input',()=>{
+    h2.innerText =  input.value
+  })
 }
 modal();
 const botao = document.getElementById("button");
 botao.addEventListener("click", function (e) {
   const input = document.querySelector("input").value;
-  if (input == "") {
-    alert("Precisa Inserir um nome");
-  } else {
-    e.preventDefault(); // botao de enviar no input onde ele redireciona para o jogo
-    hide();
-    createLayout();
-    ApertarStart();
-  }
+  input == "" ? alert("Precisa Inserir um nome") : e.preventDefault();   hide(); createLayout(); 
 });
+
+botao.addEventListener("click", function ApertarStart() {
+    feedBack();
+    if (dataGame.botaoAcionado == true) {
+      dataGame.botaoAcionado = false;
+      randomColors();
+      setTimeout(() => {}, 400);
+      setTimeout(remove, 500);;
+    }
+  });
 function hide() {
-  const div = document.querySelector(".divModal"); // função que esconde a div da primeira pagina para mostrar o tabuleiro.
-  div.classList.remove("divModal");
-  div.classList.add("hide");
+  const div = document.querySelector(".divModal");
+  div.classList.replace("divModal", "hide");
 }
 function createLayout() {
   const main = document.getElementById("main");
@@ -56,9 +69,6 @@ function createLayout() {
   p.id = "feed";
   p.innerText = "Bem vindo(a) " + `${valor}`;
   const button = document.createElement("button");
-  button.classList.add("button");
-  button.id = "start";
-  button.innerText = "START";
   const score = document.createElement("div");
   score.classList.add("score");
   score.innerText = `${dataGame.score}`;
@@ -67,7 +77,25 @@ function createLayout() {
   divContador.id = "contador";
   arrayCores.push(divBlue, divYellow, divGreen, divVermelho);
   //função clickar nas divs coloridas
-  for (let i = 0; i < arrayCores.length; i++) {
+  for (let i in arrayCores) {
+    arrayCores[i].addEventListener("click", (e)=>{
+      const clicado = e.target.id
+      console.log(clicado)
+      if (clicado == 0){
+          const audio = new Audio ("audio/audio_simonSound1.mp3")
+          audio.play()
+      }else if (clicado == 1){
+        const audio = new Audio ("audio/audio_simonSound2.mp3")
+          audio.play()
+      }else if (clicado == 2){
+        const audio = new Audio ("audio/audio_simonSound3.mp3")
+          audio.play()
+      }else{
+        const audio = new Audio ("audio/audio_simonSound4.mp3")
+          audio.play()
+      }
+
+    })
     arrayCores[i].addEventListener("click", () => {
       if (dataGame.podeApertar == true) {
         dataGame.sequenciaJogada.push(arrayCores[i].id);
@@ -76,14 +104,13 @@ function createLayout() {
             dataGame.sequenciaSorteada.length &&
           dataGame.sequenciaSorteada.every(
             (value, index) => value == dataGame.sequenciaJogada[index]
-          ) == true
+          ) == true 
         ) {
           dataGame.sequenciaJogada = [];
           setTimeout(continuarJogando, 1000);
         } else {
           dataGame.sequenciaJogada.forEach(function (elemento, indice) {
             if (elemento != dataGame.sequenciaSorteada[indice]) {
-              console.log(elemento, dataGame.sequenciaSorteada[indice]);
               quadro();
             }
           });
@@ -99,7 +126,6 @@ function createLayout() {
   section.appendChild(divCircle);
   divCircle.appendChild(score);
   divCircle.appendChild(p);
-  section.appendChild(button);
   section.appendChild(divContador);
 }
 const dataGame = {
@@ -116,73 +142,75 @@ function randomColors() {
     const numeroSorteado = Math.floor(Math.random() * 4);
     dataGame.sequenciaSorteada.push(numeroSorteado);
   }
-  for (let j = 0; j < dataGame.sequenciaSorteada.length; j++) {
+  for (let j in dataGame.sequenciaSorteada) {
     setTimeout(() => {
-      if (dataGame.sequenciaSorteada[j] === 0) {
-        arrayCores[0].classList.replace("blue", "lightBlue");
-      }
-      if (dataGame.sequenciaSorteada[j] === 1) {
-        arrayCores[1].classList.replace("yellow", "lightYellow"); //função que randomiza as cores
-      }
-      if (dataGame.sequenciaSorteada[j] === 2) {
-        arrayCores[2].classList.replace("green", "lightGreen");
-      }
-      if (dataGame.sequenciaSorteada[j] === 3) {
-        arrayCores[3].classList.replace("red", "lightRed");
+      switch (dataGame.sequenciaSorteada[j]) {
+        case 0:
+          arrayCores[0].classList.replace("blue", "lightBlue");  
+          setTimeout(()=>{
+            const audio = new Audio ("audio/audio_simonSound1.mp3")
+            audio.play()
+          },100)
+          break;
+        case 1:
+          arrayCores[1].classList.replace("yellow", "lightYellow");
+          setTimeout(()=>{
+            const audio = new Audio ("audio/audio_simonSound1.mp3")
+            audio.play()
+          },100)
+          break;
+        case 2:
+          arrayCores[2].classList.replace("green", "lightGreen");
+          setTimeout(()=>{
+          const audio3 = new Audio ("audio/audio_simonSound3.mp3")
+          audio3.play()
+          },100)
+          break;
+        case 3:
+          arrayCores[3].classList.replace("red", "lightRed");
+          setTimeout(()=>{
+            const audio4 = new Audio ("audio/audio_simonSound4.mp3")
+          audio4.play()
+          },100)   
+        break;
       }
     }, j * 1000);
   }
 }
 function remove() {
-  for (let j = 0; j <= dataGame.sequenciaSorteada.length; j++) {
+  for (let j in dataGame.sequenciaSorteada) {
     setTimeout(() => {
-      if (dataGame.sequenciaSorteada[j] === 0) {
-        arrayCores[0].classList.replace("lightBlue", "blue");
-      }
-      if (dataGame.sequenciaSorteada[j] === 1) {
-        arrayCores[1].classList.replace("lightYellow", "yellow");
-      }
-      //função que faz o tabuleiro brilhar
-      if (dataGame.sequenciaSorteada[j] === 2) {
-        arrayCores[2].classList.replace("lightGreen", "green");
-      }
-      if (dataGame.sequenciaSorteada[j] === 3) {
-        arrayCores[3].classList.replace("lightRed", "red");
+      switch (dataGame.sequenciaSorteada[j]) {
+        case 0:
+          arrayCores[0].classList.replace("lightBlue", "blue");
+        break;
+        case 1:
+          arrayCores[1].classList.replace("lightYellow", "yellow");
+        break;
+        case 2:
+          arrayCores[2].classList.replace("lightGreen", "green");
+        break;
+        case 3:
+          arrayCores[3].classList.replace("lightRed", "red");
+        break;
       }
     }, j * 1000);
   }
   return (dataGame.podeApertar = true);
 }
 function continuarJogando() {
-  const contador = document.getElementById("contador");
+  const contador = document.getElementById("contador");  
   const score = document.querySelector(".score");
   textoAcerto("Ótimo, ");
   setTimeout(() => {
     textoAcerto("Atenção para a proxima sequencia, ");
   }, 1000);
   setTimeout(randomColors, 1000);
-  //setTimeout(() => {}, 400); //função que continua quando acerta a sequencia
   setTimeout(remove, 1500);
-  dataGame.jogadas += 1;
-  dataGame.score += 1000;
+  dataGame.jogadas ++;
+  dataGame.score += 10;
   score.innerText = "Score: " + dataGame.score;
   contador.innerText = dataGame.jogadas;
-}
-function ApertarStart() {
-  const comecar = document.getElementById("start");
-  comecar.addEventListener("click", () => {
-    feedBack();
-    if (dataGame.botaoAcionado == true) {
-      dataGame.botaoAcionado = false;
-      const contador = document.getElementById("contador"); //botao start para comecar
-      contador.innerText = dataGame.jogadas;
-      randomColors();
-      setTimeout(() => {}, 400);
-      setTimeout(remove, 500);
-      // dataGame.jogadas += 1;
-      dataGame.score += 1000;
-    }
-  });
 }
 function quadro() {
   const section = document.querySelector("section");
@@ -190,10 +218,10 @@ function quadro() {
   div.classList.add("divQuadro");
   const p1 = document.createElement("p");
   p1.classList.add("frasedoQuadro");
-  p1.innerText = "Que pena, voce errou";
-  const p2 = document.createElement("p"); //quadro que aparece quando perde com botao de recomecar
+  p1.innerText = "Que pena, voce errou!";
+  const p2 = document.createElement("p");
   p2.classList.add("scoreDoQuadro");
-  p2.innerText = "Score: " + `${dataGame.score}`;
+  p2.innerText = `Score:  ${dataGame.score}`;
   const button = document.createElement("button");
   button.id = "recomecar";
   button.innerText = "Recomeçar";
@@ -201,26 +229,27 @@ function quadro() {
   div.appendChild(p2);
   div.appendChild(button);
   section.appendChild(div);
+
   button.addEventListener("click", function () {
-    div.classList.replace("divQuadro", "hide");
-    ApertaRecomecar();
+    const div = document.querySelector(".divQuadro")
+    div.classList.toggle("hide");
+  ApertaRecomecar();
   });
 }
-function ApertaRecomecar() {
+  function ApertaRecomecar() {
   const contador = document.getElementById("contador");
   const score = document.querySelector(".score");
   dataGame.sequenciaJogada = [];
   dataGame.sequenciaSorteada = [];
   dataGame.jogadas = 1;
-  dataGame.score = 0; //botao start para recomecar
+  dataGame.score = 0; 
   retornarAoTexto();
   contador.innerText = dataGame.jogadas;
   score.innerText = dataGame.score;
   randomColors();
   setTimeout(() => {}, 400);
   setTimeout(remove, 500);
-  //dataGame.jogadas += 1;
-  dataGame.score += 1000;
+
 }
 function feedBack() {
   const valor = document.querySelector("input").value;
@@ -231,10 +260,11 @@ function retornarAoTexto() {
   const valor = document.querySelector("input").value;
   const p1 = document.getElementById("feed");
   p1.innerText = "Bem vindo " + `${valor}`;
-  setTimeout(feedBack, 1000);
+  //setTimeout(feedBack, 1000);
 }
 function textoAcerto(txt) {
-  const valor = document.querySelector("input").value;
+  const valor = document.querySelector("input"). value;
   const p1 = document.getElementById("feed");
   p1.innerText = txt + `${valor}`;
 }
+
